942ad3431fc343d03956676811af414f9f99e22d
tsukkery <dariya_first@mail.ru>
2025-02-16 21:14:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
+import React, {Component} from 'react';
+import Header from "./Header/Header";
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//       Header
+//   );
+// }
+
+const App = () => {
+    return (
+        <div>
+            <Header /> {/* Используем компонент Header */}
+            {/*<h1>Наши Велосипеды</h1>*/}
+            {/* Остальной контент вашего приложения */}
+        </div>
+    );
+};
 
 export default App;
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//         <div className={App}>
+//           <Header />
+//         </div>
+//     )
+//   }
+// }
+//
+// export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
